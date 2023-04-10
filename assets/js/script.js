907e49aba162314d55f03b979c6349315f30fd7e
Alex Sanchez <tacoplayson@gmail.com>
2023-04-09 22:45:22 -0700

    edits made to html, javascript, readme, and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,31 +70,31 @@ function initPage() {
 
   var init10 = JSON.parse(localStorage.getItem("10:00 am"))
   tenAm.val(init10);
-
+  
   var init11 = JSON.parse(localStorage.getItem("11:00 am"))
   elevenAm.val(init11);
-
+  
   var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
   twelvePm.val(init12);
-
+  
   var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
   onePm.val(init1);
-
+  
   var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
   twoPm.val(init2);
-
+  
   var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
   threePm.val(init3);
-
+ 
   var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
   fourPm.val(init4);
-
+  
   var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
   fivePm.val(init5);
-
+  
   var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
   sixPm.val(init6);
-
+  
   var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
   sevenPm.val(init7);
 
@@ -112,7 +112,7 @@ function initPage() {
 } 
 
 function background () {
-
+      
   $(".form-control").each(function () {
       var timeTest = parseInt($(this).attr("id"));
       hour = parseInt(hour);
